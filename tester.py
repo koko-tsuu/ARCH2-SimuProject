2dd42aff7f4362653a5d541e7e39445df6c6351c
nemurinoU <63938840+nemurinoU@users.noreply.github.com>
2024-03-16 19:29:46 +0800

    add flask
    not done

--- tester.py
+++ tester.py
@@ -1,11 +1,9 @@
 import main_helpers
 import sub_helpers
+from flask import Flask
 
-def main():
+if __name__ == '__main__':
     # need to handle negative (check if '-' at first pos)
     # bawal floating points ata for exponent?
     sBinary = main_helpers.inputDecimalBase10("2132", "-5")
     print(sBinary)
-
-if __name__ == '__main__':
-    main()
