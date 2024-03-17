cdab0c845aa65cfbd72e7f789c427c39b4daa659
nemurinoU <63938840+nemurinoU@users.noreply.github.com>
2024-03-17 09:13:35 +0800

    added templates

--- tester.py
+++ tester.py
@@ -1,17 +1,45 @@
 import main_helpers
 import sub_helpers
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return "Hello, World!"
+    res="" if request.args.get('result') is None else request.args.get('result')
+    return render_template('index.html', result=res)
+
+@app.route("/submit", methods=['POST'])
+def calc():
+    if request.method == 'POST':
+        bmantissa = request.form['bmantissa']
+        exp = request.form['exp']
+
+        sBinary = main_helpers.inputDecimalBase10(bmantissa, exp)
+        return redirect(url_for('index'), code=307)
 
 if __name__ == '__main__':
     # need to handle negative (check if '-' at first pos)
     # bawal floating points ata for exponent?
-    sBinary = main_helpers.inputDecimalBase10("2132", "-5")
-    print(sBinary)
     app.run(port=5000, debug=True)
     
 
+"""
+def entry_page():
+    if request.method == 'POST':
+        date = request.form['date']
+        title = request.form['blog_title']
+        post = request.form['blog_main']
+        post_entry = models.BlogPost(date = date, title = title, post = post)
+        db.session.add(post_entry)
+        db.session.commit()
+        return redirect(url_for('database'))
+    else:
+        return render_template('entry.html')
+
+@app.route('/database')        
+def database():
+    query = []
+    for i in session.query(models.BlogPost):
+        query.append((i.title, i.post, i.date))
+    return render_template('database.html', query = query)
+"""
