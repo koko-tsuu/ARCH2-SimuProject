c3f707e928950cc8f16f9b50d14904d1f7aedeb9
nemurinoU <63938840+nemurinoU@users.noreply.github.com>
2024-03-17 08:22:24 +0800

    add flask localhost

--- tester.py
+++ tester.py
@@ -1,9 +1,17 @@
 import main_helpers
 import sub_helpers
 from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     # need to handle negative (check if '-' at first pos)
     # bawal floating points ata for exponent?
     sBinary = main_helpers.inputDecimalBase10("2132", "-5")
     print(sBinary)
+    app.run(port=5000, debug=True)
+    
+
